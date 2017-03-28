9c6cc9ac9e12be3087b62413740e23af9058ec15
nyletara <iguru95@gmail.com>
2017-03-28 16:43:21 -0400

    Set up endpoint without SQS

--- payment/application.py
+++ payment/application.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template, request, send_from_directory, send_file, jsonify,app, session
+from flask import Flask, render_template, request, jsonify,app, session
 import stripe
 import os
-from flask_jwt import JWT, jwt_required, current_identity
-from werkzeug.security import safe_str_cmp
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 
 stripe_keys = {
@@ -15,15 +13,14 @@ stripe.api_key = stripe_keys['secret_key']
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://p1backend:project1databasepasswordformad@p1madinstance.cukjopk8vvdd.us-west-2.rds.amazonaws.com:5432/madp1db'
-db = SQLAlchemy(application)
+# application.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://p1backend:project1databasepasswordformad@p1madinstance.cukjopk8vvdd.us-west-2.rds.amazonaws.com:5432/madp1db'
+# db = SQLAlchemy(application)
 
 application.debug = True
 application.config['SECRET_KEY'] = 'super-secret'
         
 #stripe
 @application.route('/charge', methods=['POST'])
-@jwt_required()
 def charge():
     # Amount in cents
     amount = request.form["amount"]
@@ -40,7 +37,7 @@ def charge():
         currency='usd',
         description='Flask Charge'
     )
-    
+
     result = {}
     result['status'] = charge['status']
     result['amount'] = charge['amount']
