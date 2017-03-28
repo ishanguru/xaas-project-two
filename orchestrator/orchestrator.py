39077f9d13eaacaaa6759bd4c13b6d9b5f40ed65
Brennan Wallace <brennanwallace@dyn-160-39-134-244.dyn.columbia.edu>
2017-03-28 16:20:39 -0400

    have ochestrator connected to the queue

--- orchestrator/orchestrator.py
+++ orchestrator/orchestrator.py
@@ -1,6 +1,13 @@
+import requests 
+import boto3
+import json
+
 def lambda_handler(event, context):
     method = event["method"]
     if method == "login":
+        sqs = boto3.resource('sqs')
+        queue = sqs.get_queue_by_name(QueueName='microserviceQueue')
+        queue.send_message(MessageBody=json.dumps(event))
         return "login"
     elif method == "logout":
         return "logout"
