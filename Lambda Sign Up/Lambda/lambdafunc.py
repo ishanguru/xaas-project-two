b303ce565f15840df095da427b76ee7530df2be3
Nachiket <nmp2139@columbia.edu>
2017-03-29 02:19:24 -0400

    Trying to add verification email functionality

--- Lambda Sign Up/Lambda/lambdafunc.py
+++ Lambda Sign Up/Lambda/lambdafunc.py
@@ -3,88 +3,44 @@ import pymongo
 from pymongo import MongoClient
 from flask_pymongo import PyMongo
 
+import email
+
 conn = MongoClient('mongodb://Gunnernet:nachiket_99@ds147069.mlab.com:47069/userdb')
-
 db = conn.userdb
-#mongo = PyMongo(conn)
-
-def login():
-
-    users = db.users
-    login_user = users.find_one({'name' : request.form['inputEmail']})
-
-    if login_user:
-        if request.form['inputPassword'] == login_user['password'] :
-            session['inputEmail'] = request.form['inputEmail']
-            email = request.form['inputEmail']
-
-            # payload = {'iss': email}
-            payload = {'iss': email, 'exp': 30000000000, 'admin': True}
-
-            token = jwt.encode(
-            payload,
-            application.config.get('SECRET_KEY'),
-            algorithm='HS256')
-
-            tokentoken = token
-
-            userhistory = db.userhistory
-            currentHistory = list(userhistory.find({"name" : email}))
-
-# def register():
-#     print('registering user')
-#     if request.method == 'POST':
-#         try:
-#             users = mongo.db.users
-#             existing_user = users.find_one({'name' : request.form['inputEmail']})
-#         except:
-#             print "failed to find user"
-#
-#         if existing_user is None:
-#             print('creating user')
-#             hashpass = request.form['inputPassword']
-#             users.insert({'name' : request.form['inputEmail'], 'password' : hashpass})
-#             session['inputEmail'] = request.form['inputEmail']
-#             email = request.form['inputEmail']
-#
-#             # payload = {'iss': email}
-#             payload = {'iss': email, 'exp': 30000000000, 'admin': True}
-#
-#             token = jwt.encode(
-#             payload,
-#             application.config.get('SECRET_KEY'),
-#             algorithm='HS256')
-#
-#             tokentoken = token
-#
-#             userhistory = mongo.db.userhistory
-#             currentHistory = list(userhistory.find({"name" : email}))
-#
-#             return render_template('index3.html', email=email, token=token, history=currentHistory)
-#
-#         return 'That inputEmail already exists!'
-# #
 
 
+EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
+EMAIL_HOST_USER = 'AKIAJZRYVG2CUXLHPR6A'
+EMAIL_HOST_PASSWORD = 'AjkRZ0Cav+/iNKubL+yfCpudvr2NA/YkRZ4myZ4eU6qB'
+EMAIL_PORT = 587
 
 
+def verificationemail():
+    # verificationlink =
+
+    s = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+    s.starttls()
+    s.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
 
 def lambda_handler(event, context):
+
     def register():
         print('registering user')
         if request.method == 'POST':
             try:
                 users = db.users
-                existing_user = users.find_one({'name' : request.form['inputEmail']})
+                existing_user = users.find_one({'name' : request.form['username']})
             except:
                 print ("failed to find user")
 
             if existing_user is None:
                 print('creating user')
-                hashpass = request.form['inputPassword']
-                users.insert({'name' : request.form['inputEmail'], 'password' : hashpass})
-                session['inputEmail'] = request.form['inputEmail']
-                email = request.form['inputEmail']
+                hashpass = request.form['password']
+                users.insert({'name' : request.form['username'], 'password' : hashpass})
+                session['name'] = request.form['username']
+                #email = request.form['inputEmail']
 
                 payload = {'iss': email, 'exp': 30000000000, 'admin': True}
 
@@ -95,8 +51,8 @@ def lambda_handler(event, context):
 
                 tokentoken = token
 
-                userhistory = db.userhistory
-                currentHistory = list(userhistory.find({"name" : email}))
+                #userhistory = db.userhistory
+                #currentHistory = list(userhistory.find({"name" : email}))
 
 
             return 'That inputEmail already exists!'
