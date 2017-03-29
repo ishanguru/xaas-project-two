d12d963d9e0761ec23011696179dcef5e64258dc
Brennan Wallace <brennanwallace@dyn-160-39-134-108.dyn.columbia.edu>
2017-03-29 18:24:46 -0400

    added processing status display handler

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -3,6 +3,48 @@
 
 var jwt_token = null; //recived from server, passed back doing payment
 
+var processingDisplayHandler = {
+   processing: false,
+   processingDisplayElement: $("#processingDisplay")
+};
+
+processingDisplayHandler.waiting = function () {
+    this.processingDisplayElement.text("Processing...");
+    this.processingDisplayElement.css("color","yellow");
+    this.processingDisplayElement.show();
+};
+
+processingDisplayHandler.error = function (error) {
+    this.processingDisplayElement.text("Error: " + error);
+    this.processingDisplayElement.css("color","red");
+    this.processingDisplayElement.show();
+};
+
+processingDisplayHandler.success = function (msg) {
+    this.processingDisplayElement.text("Success: " + msg);
+    this.processingDisplayElement.css("color","green");
+    this.processingDisplayElement.show();
+};
+
+processingDisplayHandler.clear = function () {
+    this.processingDisplayElement.hide()
+};
+
+
+
+var processingHandler = {
+    status : "clear",
+    displayHandler : processingDisplayHandler,
+};
+
+processingHandler.updateStatus = function (status, msg) {
+    this.status = status;
+    this.displayHandler[this.status](msg);
+
+}
+
+
+
 var accountDisplayHandler = {
     userName: null,
     loginNavElement: $("#loginNavElement"),
@@ -159,8 +201,6 @@ var handler2 = StripeCheckout.configure({
   }
 });
 
-
-
 $(document).ready(function() {
     accountDisplayHandler.logOut();
 
@@ -184,7 +224,7 @@ $(document).ready(function() {
             alert("Both password entries must match.");
             return;
         } else if (formData["password"].length < 4) {
-            alert("Passwords must be at least 5 character in length.");
+            alert("Passwords must be at least 4 character in length.");
             return;
         }
         signUp(formData);
