636a411b478b624b60400ec96c4e8e2bef7c2b0a
Nachiket <nmp2139@columbia.edu>
2017-03-29 21:16:11 -0400

    Adding a message to SQS

--- lambda_signup/Lambda/lambdafunc.py
+++ lambda_signup/Lambda/lambdafunc.py
@@ -5,10 +5,11 @@ from flask_pymongo import PyMongo
 import email
 import jwt
 
-conn = MongoClient('mongodb://Gunnernet:nachiket_99@ds147069.mlab.com:47069/userdb')
-db = conn.userdb
+## Connection with the MongoDB
+connectdb = MongoClient('mongodb://Gunnernet:nachiket_99@ds147069.mlab.com:47069/userdb')
+db = connectdb.userdb
 
-
+## Configuration for the Amazon SES service
 EMAIL_HOST = 'email-smtp.us-east-1.amazonaws.com'
 EMAIL_HOST_USER = 'AKIAJZRYVG2CUXLHPR6A'
 EMAIL_HOST_PASSWORD = 'AjkRZ0Cav+/iNKubL+yfCpudvr2NA/YkRZ4myZ4eU6qB'
@@ -16,50 +17,52 @@ EMAIL_PORT = 587
 
 
 def verificationemail():
-    # verificationlink =
-
     s = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
     s.starttls()
     s.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
     s.sendmail(me, you, msg.as_string())
     s.quit()
 
+## Set up existing AWS SQS
+conf = {
+  "sqs-access-key": "AKIAJN3ACGV3J6SG3Q5A",
+  "sqs-secret-key": "amepI5y7KFJ0PpjiC5TNiai7OFjcpnRH+39k6jqL",
+  "sqs-queue-name": "sample_queue1",
+  "sqs-region": "us-east-1",
+  "sqs-path": "sqssend"
+}
+
+import boto.sqs
+from boto.sqs.message import RawMessage
+
+conn = boto.sqs.connect_to_region(
+        conf.get('sqs-region'),
+        aws_access_key_id   = conf.get('sqs-access-key'),
+        aws_secret_access_key   = conf.get('sqs-secret-key')
+)
+
+
+q = conn.create_queue(conf.get('sqs-queue-name'))
+
 def lambda_handler(event, context):
 
-    #def register():
-    print('registering user')
-    try:
-        users = db.users
-        existing_user = users.find_one({'name' : event['username']})
-    except:
-        print ("failed to find user")
+    users = db.users
+    existing_user = users.find_one({'name' : event['username']})
 
-    #if existing_user is None:
-    print('creating user for', event['username'] )
-    hashpass = event['password']
-    users.insert({'name' : event['username'], 'password' : hashpass})
 
-    #session['name'] = event['username']
-    #email = request.form['inputEmail']
+    if existing_user:
+        m = RawMessage()
+        m.set_body('FALSE')
+        retval = q.write(m)
+        return 'That inputEmail already exists!'
+    else:
+        print('creating user for', event['username'] )
+        hashpass = event['password']
+        users.insert({'name' : event['username'], 'password' : hashpass})
+        m = RawMessage()
+        m.set_body('TRUE')
+        retval = q.write(m)
 
-    payload = {'iss': email, 'exp': 30000000000, 'admin': True}
-
-    # token = jwt.encode(
-    # payload, 'SECRET_KEY',
-    # algorithm='HS256')
-    #
-    # tokentoken = token
-    return " Successful Registration"
+        return " Successful Registration"
             #userhistory = db.userhistory
             #currentHistory = list(userhistory.find({"name" : email}))
-
-
-    return 'That inputEmail already exists!'
-    #
-
-    #application.config.get('SECRET_KEY'),
-
-
-
-
-#    return "Added %d items from RDS MySQL table" %(item_count)
