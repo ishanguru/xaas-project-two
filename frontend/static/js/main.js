a3a47d0b4b9de3755c4268263cdb20435ffecdf3
Brennan Wallace <brennanwallace@dyn-160-39-134-108.dyn.columbia.edu>
2017-03-29 20:09:46 -0400

    added a poll function wo a url

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -4,7 +4,46 @@
  */
 
 
+function poll(count, type) {
 
+    function handleFailure(count, data) {
+        if (count <= 4) { //try again
+            var timeoutID = window.setTimeout(poll(count + 1), 200);
+        } else  {
+            //todo: probably want to send this info somewhere
+            processingHandler.updateStatus("failure", "That operation failed please try again.");
+        }
+    }
+
+
+    data["jwt"] = accountHandler.jwt_token;
+    data["type"] = type;
+
+    $.ajax({
+        type: "POST",
+        url: "tbd",
+        data: data,
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+            if (data["status"] === "success") {
+                processingHandler.updateStatus("success", "That operation worked!");
+            } else {
+                handleFailure(count,data)
+            }
+        },
+        error: function (data) {
+            handleFailure(count,data)
+        }
+    });
+
+    if (true) {
+
+    } else if (count < 2) { //try again
+        var timeoutID = window.setTimeout(poll(count + 1), 200);
+    } else  {
+        processingHandler.updateStatus("failure", "That operation failed please try again.");
+    }
+}
 
 function login(formData) {
     formData["method"] = "login";
