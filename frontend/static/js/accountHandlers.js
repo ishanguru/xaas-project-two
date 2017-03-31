dce912dd4130e156b06662ab2bf4edc59d48421f
Brennan Wallace <brennanwallace@dyn-160-39-134-108.dyn.columbia.edu>
2017-03-31 00:04:45 -0400

    moved account handlers to their own file

---
+++ frontend/static/js/accountHandlers.js
@@ -0,0 +1,41 @@
+function handleLogin(formData) {
+    formData["method"] = "login";
+    $('#loginModal').modal('hide');
+    $.ajax({
+        type: "POST",
+        url: 'https://ibw5jd0k4c.execute-api.us-east-1.amazonaws.com/orchestratorV2/ui-api',
+        data: JSON.stringify(formData),
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+          var info = {
+            username: formData.username,
+            password: formData.password
+          };
+          startPollLogin(info)
+        },
+        error: function (data) {
+            alert("Login Failure")
+        }
+    });
+}
+
+function handleSignup(formData) { //new acccount
+    formData["method"] = "signup";
+    $('#signUpModal').modal('hide')
+    $.ajax({
+        type: "POST",
+        url: 'https://ibw5jd0k4c.execute-api.us-east-1.amazonaws.com/orchestratorV2/ui-api',
+        data: JSON.stringify(formData),
+        success: function(data) {
+          var info = {
+            username: formData.username,
+            password: formData.password
+          };
+          startPollSignup(info);
+        },
+        error: function (data) {
+            alert("Could not sign up. Username may be taken.");
+        }
+    });
+}
