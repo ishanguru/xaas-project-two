26b2a2d78bd88085c14fb2711039c23393dd45a4
Brennan Wallace <brennanwallace@dyn-160-39-134-108.dyn.columbia.edu>
2017-03-31 00:08:15 -0400

    trying to deal with email username diff

--- frontend/static/js/accountHandlers.js
+++ frontend/static/js/accountHandlers.js
@@ -4,7 +4,7 @@ function handleLogin(formData) {
     $.ajax({
         type: "POST",
         url: 'https://ibw5jd0k4c.execute-api.us-east-1.amazonaws.com/orchestratorV2/ui-api',
-        data: JSON.stringify(formData),
+        data: JSON.stringify(Object.assign(formData,{"username" : formData["Email"]})),
         dataType : "json",
         contentType: "application/json; charset=utf-8",
         success: function(data) {
@@ -26,7 +26,7 @@ function handleSignup(formData) { //new acccount
     $.ajax({
         type: "POST",
         url: 'https://ibw5jd0k4c.execute-api.us-east-1.amazonaws.com/orchestratorV2/ui-api',
-        data: JSON.stringify(formData),
+        data: JSON.stringify(JSON.stringify(Object.assign(formData,{"username" : formData["Email"]}))),
         success: function(data) {
           var info = {
             username: formData.username,
