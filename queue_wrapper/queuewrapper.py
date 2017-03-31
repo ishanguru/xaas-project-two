17ceafb3b46fa779f7e31b64311afa0fa753bbce
Brennan Wallace <brennanwallace@dyn-160-39-134-112.dyn.columbia.edu>
2017-03-31 16:40:56 -0400

    updated queue wrapper

--- queue_wrapper/queuewrapper.py
+++ queue_wrapper/queuewrapper.py
@@ -8,24 +8,64 @@ except Exception as e:
 	print("Queue exists")
 
 def lambda_handler(event, trigger):
-	if event['type'] == 'accountsQuery':
-		response = processAccountsData(event)
-	elif event['type'] == 'informationQuery':
-		response = processCustomerData(event)
+	if event['type'] == 'signupQuery':
+		response = processSignupData(event)
+	elif event['type'] == 'loginQuery':
+		response = processLoginData(event)
 	elif event['type'] == 'paymentsQuery':
 		response = processPaymentsData(event)
 	else:
 		response = {'error': "Not a valid query type"}
 	return json.loads(response)
 
-def processAccountsData(event):
-	queue_name = sqs_queue.getQueueName('authenticationQueue')
+def processSignupData(event):
+
+	conf = {"sqs-access-key": "AKIAJN3ACGV3J6SG3Q5A",
+			"sqs-secret-key": "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",
+			"sqs-queue-name": "queue_signup",
+			"sqs-region": "us-east-1",
+			"sqs-path": "sqssend"
+			}
+
+	conn = boto.sqs.connect_to_region(
+		conf.get('sqs-region'),
+		aws_access_key_id=conf.get('sqs-access-key'),
+		aws_secret_access_key=conf.get('sqs-secret-key')
+	)
+
+	queue = conn.get_queue('queue_signup')
 	response = []
-	for message in queue_name.receive_messages(MessageAttributeNames=['email']):
-        json_dict = json.loads(message.body)
-        if event['jwt'] == json_dict['jwt']:
-        	response.append(message)
-    return response
+
+	for message in queue.receive_messages(MessageAttributeNames=['email']):
+		response.append(message)
+
+	return response
+
+def processLoginData(event):
+
+	conf = {"sqs-access-key": "AKIAJN3ACGV3J6SG3Q5A",
+			"sqs-secret-key": "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",
+			"sqs-queue-name": "queue_login",
+			"sqs-region": "us-east-1",
+			"sqs-path": "sqssend"
+
+			}
+
+	conn = boto.sqs.connect_to_region(
+		conf.get('sqs-region'),
+		aws_access_key_id=conf.get('sqs-access-key'),
+		aws_secret_access_key=conf.get('sqs-secret-key')
+	)
+
+	queue = conn.get_queue('queue_login')
+	response = []
+
+	for message in queue.receive_messages(MessageAttributeNames=['email']):
+		response.append(message)
+
+	return response
+
+
     
 def processCustomerData(event):
 	queue_name = sqs_queue.getQueueName('informationQueue')
