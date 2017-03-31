85d5886ef7620f32aea9a996ded66d2ce29b1781
Brennan Wallace <brennanwallace@dyn-160-39-134-112.dyn.columbia.edu>
2017-03-31 19:39:49 -0400

    added db and laid func

--- login/lambdafunc_login.py
+++ login/lambdafunc_login.py
@@ -7,22 +7,18 @@ db = connectdb.userdb
 
 import boto.sns
 
+conf = {"sqs-access-key": "AKIAJN3ACGV3J6SG3Q5A",
+        "sqs-secret-key": "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",
+        "sqs-queue-name": "queue_login",
+        "sqs-region": "us-east-1",
+        "sqs-path": "sqssend"
 
-
-conf = { "sqs-access-key": "AKIAJN3ACGV3J6SG3Q5A",
-"sqs-secret-key": "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",
-"sqs-queue-name": "queue_login",
-"sqs-region": "us-east-1",
-"sqs-path": "sqssend"
-
-}
-
-
+        }
 
 conn = boto.sqs.connect_to_region(
-        conf.get('sqs-region'),
-        aws_access_key_id   = conf.get('sqs-access-key'),
-        aws_secret_access_key   = conf.get('sqs-secret-key')
+    conf.get('sqs-region'),
+    aws_access_key_id=conf.get('sqs-access-key'),
+    aws_secret_access_key=conf.get('sqs-secret-key')
 )
 
 q = conn.get_queue('queue_login')
@@ -30,21 +26,19 @@ q = conn.get_queue('queue_login')
 import boto.ses
 
 verificationconn = boto.ses.connect_to_region(
-        'us-east-1',
-        aws_access_key_id='AKIAJN3ACGV3J6SG3Q5A',
-        aws_secret_access_key='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')
+    'us-east-1',
+    aws_access_key_id='AKIAJN3ACGV3J6SG3Q5A',
+    aws_secret_access_key='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')
 
 verifiedemails = verificationconn.list_verified_email_addresses()
 
 
 def lambda_handler(event, context):
-
     users = db.users
-    login_user = users.find_one({'name' : event['username']})
-
+    login_user = users.find_one({'name': event['username']})
 
     if login_user:
-        if event['username'] == login_user['name'] :
+        if event['username'] == login_user['name']:
             if event['password'] == login_user['password']:
                 for a in verifiedemails.values():
                     for b in a.values():
@@ -52,12 +46,19 @@ def lambda_handler(event, context):
                             for i in email:
                                 if (i == event['username']):
                                     m = RawMessage()
-                                    m.set_body(str({event['username']) : 'TRUE'})
+                                    m.set_body(str({event['username']): 'TRUE'})
                                     q.write(m)
-
+                                    connectdb.loginAttempts.findOneAndReplace({
+                                        {"_id": event["laid"]},
+                                        {"status": "success"}
+                                    })
                                     return "Successful login"
 
     m = RawMessage()
-    m.set_body({event['username'] : 'FALSE'})
+    connectdb.loginAttempts.findOneAndReplace({
+        {"_id": event["laid"]},
+        {"status": "error"}
+    })
+    m.set_body({event['username']: 'FALSE'})
     q.write(m)
     return "Invalid Username/Password"
