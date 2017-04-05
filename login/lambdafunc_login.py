ed995245665bd0236ac8910c8068e8671e46d912
Brennan Wallace <brennanwallace@Brennans-MacBook-Pro.local>
2017-04-04 20:53:22 -0400

    removed emial verfication from login seems to work

--- login/lambdafunc_login.py
+++ login/lambdafunc_login.py
@@ -7,22 +7,6 @@ import json
 connectdb = MongoClient('mongodb://user1:user1password@ds149040.mlab.com:49040/user_db')["user_db"]
 db = connectdb
 
-conf = {"sqs-access-key": "AKIAJN3ACGV3J6SG3Q5A",
-        "sqs-secret-key": "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",
-        "sqs-queue-name": "queue_login",
-        "sqs-region": "us-east-1",
-        "sqs-path": "sqssend"
-
-        }
-
-conn = boto.sqs.connect_to_region(
-    conf.get('sqs-region'),
-    aws_access_key_id=conf.get('sqs-access-key'),
-    aws_secret_access_key=conf.get('sqs-secret-key')
-)
-
-q = conn.get_queue('queue_login')
-
 verificationconn = boto.ses.connect_to_region(
     'us-east-1',
     aws_access_key_id='AKIAIYILDTN5HSKGIAOA',
@@ -48,25 +32,35 @@ def lambda_handler(event, context):
     print (event)
     event = json.loads(str(event))
     print (type(event))
-    login_user = users.find_one({'name': event['username']})
-
-
+    login_user = users.find_one({'name': event['username'], 'password': event['password']})
     if login_user:
-        if event['username'] == login_user['name']:
-            if event['password'] == login_user['password']:
-                for a in verifiedemails.values():
-                    for b in a.values():
-                        for email in b.values():
-                            for i in email:
-                                if (i == event['username']):
-                                    connectdb.loginAttempts.find_one_and_replace(
-                                        {"_id": ObjectId(event["aid"])},
-                                        {"status": "success"}
-                                    )
-                                    return "Successful login"
-
-    connectdb.loginAttempts.find_one_and_replace(
-        {"_id": ObjectId(event["aid"])},
-        {"status": "error"}
-    )
-    return "Invalid Username/Password"
+        connectdb.loginAttempts.find_one_and_replace(
+            {"_id": ObjectId(event["aid"])},
+            {"status": "success"}
+        )
+        return "Successful login"
+    else:
+        connectdb.loginAttempts.find_one_and_replace(
+            {"_id": ObjectId(event["aid"])},
+            {"status": "error"}
+        )
+        return "Invalid Username/Password"
+    # if login_user:
+    #     if event['username'] == login_user['name']:
+    #         if event['password'] == login_user['password']:
+    #             for a in verifiedemails.values():
+    #                 for b in a.values():
+    #                     for email in b.values():
+    #                         for i in email:
+    #                             if (i == event['username']):
+    #                                 connectdb.loginAttempts.find_one_and_replace(
+    #                                     {"_id": ObjectId(event["aid"])},
+    #                                     {"status": "success"}
+    #                                 )
+    #                                 return "Successful login"
+    #
+    # connectdb.loginAttempts.find_one_and_replace(
+    #     {"_id": ObjectId(event["aid"])},
+    #     {"status": "error"}
+    # )
+    # return "Invalid Username/Password"
