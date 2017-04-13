681a19b685c9aa829d6611f2d0be4b736f60f427
Brennan Wallace <b.wallace@columbia.edu>
2017-04-13 17:21:33 -0400

    made the flask app an lambda function.

--- verification/lambdafunc_verify.py
+++ verification/lambdafunc_verify.py
@@ -1,23 +1,6 @@
-from pymongo import MongoClient
-import boto.ses
-from bson.objectid import ObjectId
-import json
-
-connectdb = MongoClient('mongodb://user1:user1password@ds149040.mlab.com:49040/user_db')["user_db"]
-db = connectdb
-
-verificationconn = boto.ses.connect_to_region(
-        'us-east-1',
-        aws_access_key_id='',
-        aws_secret_access_key='')
-
 def lambda_handler(event, context):
-    verification = event["username"]
-
-    verificationconn.send_email(
-            'nachi.2605@gmail.com',
-            'Verify your email',
-            'Enter the Lambda function here',
-            [verification])
-
-    return ("Verification mail has been sent")
+    # TODO implement
+    userId = event["params"]["path"]["userId"]
+    if userId[0] == '"' and userId[-1] == '"':
+        userId = userId[1:-1]
+    return userId
