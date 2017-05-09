7b1c6b8c0e5e468bb5598469658f778576295fd1
nyletara <iguru95@gmail.com>
2017-05-08 22:59:08 -0400

    Creating payments queue wrapper

---
+++ payments_queue_wrapper/payments_wrapper.py
@@ -0,0 +1,29 @@
+import boto3
+import ConfigParser
+
+config_file_path = os.environ['LAMBDA_TASK_ROOT'] + '/configurations.txt'
+
+config = ConfigParser.ConfigParser()
+config.readfp(open(config_file_path))
+
+awsAccessKey=config.get('AWS Keys', 'awsAccessKeyId')
+awsSecretKey=config.get('AWS Keys', 'awsSecretAccessKeyId')
+awsRegion=config.get('AWS Keys', 'awsRegion')
+
+aws_session = Session(
+    aws_access_key_id=awsAccessKey,
+    aws_secret_access_key=awsSecretKey,
+    region_name=awsRegion,
+)
+
+try:
+	sqs_queue = aws_session.client('sqs')
+except Exception as e:
+    print("Queue already exists")
+
+def payments_handler(event, context):
+	queue_url = sqs_queue.get_queue_url(QueueName='ordersQueue')
+	sqs_queue.receive_message(
+		QueueUrl=queue_url,
+		
+	)	
